25f9186cc905437b31fbb76b3cdcc7fd5bad2c42
diegosanabria76 <diegosanabria76gmail.com>
2021-10-14 16:25:00 -0400

    this is second commit

---
+++ calculateSquare.py
@@ -0,0 +1,14 @@
+def calcsquare(num):
+    print(num**)
+
+
+print("More calculations")
+num1= int(input("please enter number"))
+num2= int(input("please second number"))
+
+def add(num1,num2):
+
+    return  num1 + num2
+
+
+print(add(num1,num2))
